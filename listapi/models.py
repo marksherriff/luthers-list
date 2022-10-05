2d27d968e9a51e934db8a424a0425ebf6f022504
Will-McBurney <pmcburn1@gmail.com>
2022-10-05 18:43:16 -0400

    Added first draft of models.py

--- listapi/models.py
+++ listapi/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+
+class Instructor(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.CharField(max_length=30)
+
+
+class Section(models.Model):
+    course_id = models.IntegerField
+    instructor = models.ForeignKey(Instructor, on_delete=models.CASCADE())
+    semester_code = models.IntegerField
+    course_section = models.TextField(max_length=3)
+    subject = models.TextField(max_length=4)
+    catalog_number = models.IntegerField
+    description = models.TextField(max_length=100)
+    units = models.IntegerField
+    component = models.TextField(max_length=3)
+    class_capacity = models.IntegerField
+    wait_list = models.IntegerField
+    wait_cap = models.IntegerField
+    enrollment_total = models.IntegerField
+    enrollment_available = models.IntegerField
+    topic = models.TextField(blank=True, null=True)
+
+
+class Meeting(models.Model):
+    section = models.ForeignKey(Section, on_delete=models.CASCADE())
+    days = models.TextField(max_length=10)
+    start_time = models.TextField(max_length=21)
+    facility_description = models.TextField(max_length=25)
+    end_time = models.TextField(max_length=21)
+
