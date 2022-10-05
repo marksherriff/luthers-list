437f2de09e52ce3aefdd272563b7cd4f4ce0c072
Mark Sherriff <sherriff@virginia.edu>
2022-10-05 13:22:45 -0400

    db update

--- lutherslist/settings.py
+++ lutherslist/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-on=k81!9ks97+s1k6pdwla8pgja+i_msrhk3nr)bzq+2tu%h%z
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['luthers-list.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
@@ -73,10 +73,22 @@ WSGI_APPLICATION = 'lutherslist.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'd3h7ei9kfpdrcs',
+        'USER': 'qxunmquumspehl',
+        'PASSWORD': 'e89c682002405bad684b6a826ac10e56fbbc58125712ddb421d1c531d640a8af',
+        'HOST': 'ec2-34-199-68-114.compute-1.amazonaws.com',
+        'PORT': '5432',
+
     }
 }
 
