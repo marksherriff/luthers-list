5d2d66386842a9d425e7124bed55b160859a4a74
Will-McBurney <pmcburn1@gmail.com>
2022-10-05 20:38:50 -0400

    I just want the models to work again

--- listapi/admin.py
+++ listapi/admin.py
@@ -3,6 +3,6 @@ from django.contrib import admin
 # Register your models here.
 from .models import *
 
-# admin.site.register(Instructor)
-# admin.site.register(Section)
-# admin.site.register(Meeting)
+admin.site.register(Instructor)
+admin.site.register(Section)
+admin.site.register(Meeting)
