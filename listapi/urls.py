82df1166798236d8fa1d23c39b7bed5222c4712c
Mark Sherriff <sherriff@virginia.edu>
2022-10-06 00:24:26 -0400

    fixed url

--- listapi/urls.py
+++ listapi/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('loadapi/', views.load_api, name='load_api'),
-    path('olddept/<str:dept>/', views.find_all_by_dept_old, name='find_all_by_dept_old'),
+    path('loadapi/<str:dept>/', views.load_api_by_dept, name='load_api_by_dept'),
     path('dept/<str:dept>/', views.find_all_by_dept, name='find_all_by_dept'),
+    path('deptv2/<str:dept>/', views.find_all_by_dept_v2, name='find_all_by_dept_v2'),
 ]
--- listapi/views.py
+++ listapi/views.py
@@ -11,12 +11,17 @@ def load_api(request):
     get_all_json_files()
     return HttpResponse("Hello, world. I just read a whole lot of JSON files.")
 
-def find_all_by_dept_old(request, dept):
+def load_api_by_dept(request, dept):
+    filename = 'JSON/' + dept + '.json'
+    load_json_file(filename)
+    return HttpResponse("Hello, world. I just read a whole lot of JSON files.")
+
+def find_all_by_dept(request, dept):
     sections = Section.objects.filter(subject=dept)
     sections_serialized = serializers.serialize('json', sections)
     return HttpResponse(sections_serialized, content_type='application/json')
 
-def find_all_by_dept(request, dept):
+def find_all_by_dept_v2(request, dept):
     sections = Section.objects.filter(subject=dept)
     return render(request, 'findallbydept.html', {'sections': sections})
 
