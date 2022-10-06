43cd09ea1b447546be6cb568a95359a4d351961c
Mark Sherriff <sherriff@virginia.edu>
2022-10-06 16:59:06 -0400

    cleaning up code

--- listapi/urls.py
+++ listapi/urls.py
@@ -4,12 +4,10 @@ from rest_framework import routers
 
 router = routers.DefaultRouter()
 router.register(r'sections', views.SectionViewSet)
-router.register(r'dept/(?P<dept>.+)', views.DeptViewSet)
+router.register(r'dept/(?P<dept>.+)', views.DeptViewSet, basename='dept')
 
 urlpatterns = [
     path('', include(router.urls)),
     path('loadapi/', views.load_api, name='load_api'),
     path('loadapi/<str:dept>/', views.load_api_by_dept, name='load_api_by_dept'),
-    path('dept/<str:dept>/', views.find_all_by_dept, name='find_all_by_dept'),
-    path('deptv2/<str:dept>/', views.find_all_by_dept_v2, name='find_all_by_dept_v2'),
 ]
--- listapi/views.py
+++ listapi/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponseRedirect, HttpResponse, JsonResponse
+from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, render, redirect
 from django.urls import reverse
 from django.views import generic
@@ -8,7 +8,6 @@ from .api_loader import *
 from .models import Section,Instructor,Meeting
 from .serializers import SectionSerializer, InstructorSerializer
 from rest_framework import viewsets
-from rest_framework import permissions
 
 def load_api(request):
     get_all_json_files()
@@ -17,19 +16,10 @@ def load_api(request):
 def load_api_by_dept(request, dept):
     filename = 'JSON/' + dept + '.json'
     load_json_file(filename)
-    return HttpResponse("Hello, world. I just read a whole lot of JSON files.")
-
-def find_all_by_dept(request, dept):
-    sections = Section.objects.filter(subject=dept)
-    sections_serialized = SectionSerializer(sections,many=True)
-    return HttpResponse(sections_serialized, content_type='application/json')
-
-def find_all_by_dept_v2(request, dept):
-    sections = Section.objects.filter(subject=dept)
-    return render(request, 'findallbydept.html', {'sections': sections})
+    return HttpResponse("Hello, world. I just read a JSON file for " + dept + ".")
 
 class DeptViewSet(viewsets.ModelViewSet):
-    queryset = Section.objects.all()
+    # queryset = Section.objects.all()
     serializer_class = SectionSerializer
 
     def get_queryset(self):
