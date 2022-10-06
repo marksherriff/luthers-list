a65cada850316c3be712687cf2ec46497af39583
Mark Sherriff <sherriff@virginia.edu>
2022-10-06 00:01:23 -0400

    attempt at using template - will abandon

--- listapi/urls.py
+++ listapi/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('loadapi/', views.load_api, name='load_api'),
+    path('olddept/<str:dept>/', views.find_all_by_dept_old, name='find_all_by_dept_old'),
     path('dept/<str:dept>/', views.find_all_by_dept, name='find_all_by_dept'),
 ]
--- listapi/views.py
+++ listapi/views.py
@@ -11,7 +11,15 @@ def load_api(request):
     get_all_json_files()
     return HttpResponse("Hello, world. I just read a whole lot of JSON files.")
 
+def find_all_by_dept_old(request, dept):
+    sections = Section.objects.filter(subject=dept)
+    instructors = Instructor.objects.all()
+    meetings = Meeting.objects.all()
+    all_sections_to_return = [*instructors, *meetings, *sections]
+    sections_serialized = serializers.serialize('json', all_sections_to_return)
+    return HttpResponse(sections_serialized, content_type='application/json')
+
 def find_all_by_dept(request, dept):
     sections = Section.objects.filter(subject=dept)
-    sections_list = serializers.serialize('json', sections)
-    return HttpResponse(sections_list, content_type='application/json')
+    return render(request, 'findallbydept.html', {'sections': sections})
+
