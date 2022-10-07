685e53ea5e93a3e7f118c604f33f29890431db86
Mark Sherriff <sherriff@virginia.edu>
2022-10-06 21:29:54 -0400

    serializer work

--- listapi/serializers.py
+++ listapi/serializers.py
@@ -10,7 +10,19 @@ class InstructorSerializer(serializers.ModelSerializer):
 
 class SectionSerializer(serializers.ModelSerializer):
     instructor = InstructorSerializer()
+    meetings = serializers.SlugRelatedField(
+        many=True,
+        read_only=True,
+        slug_field='days'
+    )
 
     class Meta:
         model = Section
         fields = '__all__'
+
+class MeetingSerializer(serializers.ModelSerializer):
+    section = SectionSerializer()
+    class Meta:
+        model = Meeting
+        fields = '__all__'
+
